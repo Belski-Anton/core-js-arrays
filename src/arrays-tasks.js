d8ad6b15e40700a382556aedde967e8b9cedfe8e
Anton Belski <anton.belski@gmail.com>
2025-10-12 18:00:41 +0300

    feat: add solve for getStringsLength, getAverage(

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -103,8 +103,8 @@ function removeFalsyValues(arr) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((value) => value.length);
 }
 
 /**
@@ -121,8 +121,10 @@ function getStringsLength(/* arr */) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+  const sum = arr.reduce((accum, num) => accum + num);
+  return Math.round((sum / arr.length) * 100) / 100;
 }
 
 /**
